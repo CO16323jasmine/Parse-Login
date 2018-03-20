c1a481ad0f7f86d93760cf4a2e3ffe1f5932a812
Ashlin-Siby <36127896+Ashlin-Siby@users.noreply.github.com>
2018-03-20 10:45:28 +0530

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -19,45 +19,45 @@ Parse.serverURL = 'http://18.188.102.180:80/parse/'
 
 
 
-var user = new Parse.User();
-user.save({
-  username: 'Ashlin',
-  email: 'ashlinsiby@gmail.com',
-  password: '123'
-}, {
-  success: function(response) {
-    alert('New object create with success! ObjectId: ' + response.id + `, ` + user.get('email'));
-  },
-  error: function(response, error) {
-    alert('Error: ' + error.message);
-  }
-});
-
-});
-
-
-// $('#sign-in').on('submit', function(e) {
- 
-//     // Prevent Default Submit Event
-//     e.preventDefault();
- 
-//     // Get data from the form and put them into variables
-//     var data = $(this).serializeArray(),
-//         username = data[0].value,
-//         password = data[1].value;
- 
-//     // Call Parse Login function with those variables
-//     Parse.User.logIn(username, password, {
-//         // If the username and password matches
-//         success: function(user) {
-//             alert('Welcome!');
-//         },
-//         // If there is an error
-//         error: function(user, error) {
-//             console.log(error);
-//         }
-//     });
- 
+// var user = new Parse.User();
+// user.save({
+//   username: 'Ashlin',
+//   email: 'ashlinsiby@gmail.com',
+//   password: '123'
+// }, {
+//   success: function(response) {
+//     alert('New object create with success! ObjectId: ' + response.id + `, ` + user.get('email'));
+//   },
+//   error: function(response, error) {
+//     alert('Error: ' + error.message);
+//   }
 // });
-//     });
+
+// });
+
+
+$('#sign-in').on('submit', function(e) {
+ 
+    // Prevent Default Submit Event
+    e.preventDefault();
+ 
+    // Get data from the form and put them into variables
+    var data = $(this).serializeArray(),
+        username = data[0].value,
+        password = data[1].value;
+ 
+    // Call Parse Login function with those variables
+    Parse.User.logIn(username, password, {
+        // If the username and password matches
+        success: function(user) {
+            alert('Welcome!');
+        },
+        // If there is an error
+        error: function(user, error) {
+            console.log(error);
+        }
+    });
+ 
+});
+    });
 
